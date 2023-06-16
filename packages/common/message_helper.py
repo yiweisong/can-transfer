abaa986160560c742aa6e4480dd2e8b30730e041
Song Yi Wei <ywsong@aceinna.com>
2023-06-16 09:14:34 +0800

    fix: support uart cA command for RTK350LA

--- packages/common/message_helper.py
+++ packages/common/message_helper.py
@@ -1,70 +1,1 @@
-import struct
-from typing import List
-
-COMMAND_START = [0x55, 0x55]
-
-
-def convert_mac_string_to_bytes(mac_str):
-    return bytes([int(x, 16) for x in mac_str.split(':')])
-
-
-def build_eth_command(dest_mac, src_mac, message_type, message_bytes: bytes) -> list:
-    '''
-    Build ethernet packet
-    '''
-    packet = []
-    packet.extend(message_type)
-    msg_len = len(message_bytes)
-
-    packet_len = struct.pack("<I", msg_len)
-
-    packet.extend(packet_len)
-    final_packet = packet + message_bytes
-
-    msg_len = len(COMMAND_START) + len(final_packet) + 2
-    payload_len = struct.pack('>H', len(COMMAND_START) + len(final_packet) + 2)
-
-    whole_packet = []
-    dest = convert_mac_string_to_bytes(dest_mac)
-    src = convert_mac_string_to_bytes(src_mac)
-
-    header = dest + src + bytes(payload_len)
-    whole_packet.extend(header)
-
-    whole_packet.extend(COMMAND_START)
-    whole_packet.extend(final_packet)
-    whole_packet.extend(calc_crc(final_packet))
-    if msg_len < 46:
-        fill_bytes = bytes(46-msg_len)
-        whole_packet.extend(fill_bytes)
-
-    return bytes(whole_packet)
-
-
-def calc_crc(payload):
-    '''
-    Calculates 16-bit CRC-CCITT
-    '''
-    crc = 0x1D0F
-    for bytedata in payload:
-        crc = crc ^ (bytedata << 8)
-        i = 0
-        while i < 8:
-            if crc & 0x8000:
-                crc = (crc << 1) ^ 0x1021
-            else:
-                crc = crc << 1
-            i += 1
-
-    crc = crc & 0xffff
-    crc_msb = (crc & 0xFF00) >> 8
-    crc_lsb = (crc & 0x00FF)
-    return [crc_msb, crc_lsb]
-
-
-def build_nmea_message(parts: List[str]) -> bytes:
-    payload = ','.join(parts)
-    calc_cksum = 0
-    for s in payload:
-        calc_cksum ^= ord(s)
-    return '${0}*{1:X}\r\n'.format(payload, calc_cksum).encode()
+import structfrom typing import ListCOMMAND_START = [0x55, 0x55]def convert_mac_string_to_bytes(mac_str):    return bytes([int(x, 16) for x in mac_str.split(':')])def build_eth_command(dest_mac, src_mac, message_type, message_bytes: bytes) -> list:    '''    Build ethernet packet    '''    packet = []    packet.extend(message_type)    msg_len = len(message_bytes)    packet_len = struct.pack("<I", msg_len)    packet.extend(packet_len)    final_packet = packet + message_bytes    msg_len = len(COMMAND_START) + len(final_packet) + 2    payload_len = struct.pack('>H', len(COMMAND_START) + len(final_packet) + 2)    whole_packet = []    dest = convert_mac_string_to_bytes(dest_mac)    src = convert_mac_string_to_bytes(src_mac)    header = dest + src + bytes(payload_len)    whole_packet.extend(header)    whole_packet.extend(COMMAND_START)    whole_packet.extend(final_packet)    whole_packet.extend(calc_crc(final_packet))    if msg_len < 46:        fill_bytes = bytes(46-msg_len)        whole_packet.extend(fill_bytes)    return bytes(whole_packet)def build_uart_command(message_type:bytes, message_bytes: bytes) -> list:    '''    Build uart packet    '''    packet = []    packet.extend(message_type)    msg_len = len(message_bytes)    packet.append(msg_len)    final_packet = packet + message_bytes    return COMMAND_START + final_packet + calc_crc(final_packet)def calc_crc(payload):    '''    Calculates 16-bit CRC-CCITT    '''    crc = 0x1D0F    for bytedata in payload:        crc = crc ^ (bytedata << 8)        i = 0        while i < 8:            if crc & 0x8000:                crc = (crc << 1) ^ 0x1021            else:                crc = crc << 1            i += 1    crc = crc & 0xffff    crc_msb = (crc & 0xFF00) >> 8    crc_lsb = (crc & 0x00FF)    return [crc_msb, crc_lsb]def build_nmea_message(parts: List[str]) -> bytes:    payload = ','.join(parts)    calc_cksum = 0    for s in payload:        calc_cksum ^= ord(s)    return '${0}*{1:X}\r\n'.format(payload, calc_cksum).encode()
--- packages/transfer/transfer_center.py
+++ packages/transfer/transfer_center.py
@@ -1,1 +1,246 @@
-import structimport timeimport threadingfrom typing import Any, Listfrom ..typings import EthOptions, UartOptionsfrom ..transfer.uart_transfer import UartTransferfrom ..transfer.eth_100base_t1_transfer import Eth100BaseT1Transferfrom ..devices.eth_device import (select_ethernet_interface, collect_devices)from . import (create_transfer, create_eth_100base_t1_transfer)from ..common import (message_helper, utils, app_logger)class TransferBase:    def __init__(self) -> None:        super().__init__()    def on_initalize(self):        pass    def send(self, speed: int):        passclass LG69TUartTransfer(TransferBase):    _transfer: UartTransfer    def __init__(self, options: UartOptions) -> None:        super().__init__()        self._transfer = create_transfer(options)        if not self._transfer:            raise Exception(                'Cannot connect the LG69T Uart transfer, please check the configuration')    def on_initalize(self):        # set uart transfer mode        message = message_helper.build_nmea_message(            [                'PQTMCFGDRMODE',                '1',                '2'            ])        self._transfer.send(message)        time.sleep(0.1)        # save configuration        message = message_helper.build_nmea_message(            [                'PQTMSAVEPAR'            ])        self._transfer.send(message)        # start a thread to receive message from uart        threading.Thread(target=self.__listen_data).start()    def send_vehicle_speed(self, speed: int):        # write uart message        # build NMEA format message        actual = round(speed*1000/3600,4)        message = message_helper.build_nmea_message(            [                'PQTMVEHMSG',                '1',                '0',                str(actual)            ])        self._transfer.send(message)    def __listen_data(self):        log_file = app_logger.create_raw_file_logger('lg69t_raw')        try:            while True:                data = self._transfer.recv()                # write to log file                log_file.append(data)                time.sleep(0.1)        except KeyboardInterrupt as ex:            returnclass INS401EthernetTransfer(TransferBase):    _transfer: Eth100BaseT1Transfer    _src_mac: str    _dst_mac_addresses: List[str]    def __init__(self) -> None:        super().__init__()        iface = select_ethernet_interface()        self._src_mac = iface.mac        eth_devices = collect_devices(iface, self._src_mac)        config = utils.get_config()        if config["devices_mac"]:            self._dst_mac_addresses = config["devices_mac"]        else:            self._dst_mac_addresses = [                device.mac_address for device in eth_devices]        self._transfer = create_eth_100base_t1_transfer(EthOptions(            iface,            self._src_mac,            self._dst_mac_addresses))        if not self._transfer:            raise Exception(                'Cannot connect the INS 100base-t1 transfer, please check the configuration')    def on_initalize(self):        print('[Info] Listen mac address list:')        if len(self._dst_mac_addresses) == 0:            print('[Info] No device detected')        for address in self._dst_mac_addresses:            print('MAC: {0}, SN: {1}'.format(                address, utils.convert_mac_to_sn(address)))    def send_vehicle_speed(self, speed: int):        def build_eth_commands(devices_mac, local_mac, packet_type_bytes, message_bytes):            commands = []            for dest_mac in devices_mac:                command = message_helper.build_eth_command(dest_mac, local_mac,                                                           packet_type_bytes,                                                           message_bytes)                commands.append(command)            return commands        commands = build_eth_commands(self._dst_mac_addresses,                                      self._src_mac,                                      bytes([0x01, 0x0b]),                                      list(struct.pack("<f", speed)))        if self._transfer:            self._transfer.send_batch(commands)class TransferFactory:    def create(config):        protocol = config.get('protocol')        provider = config.get('provider')        if protocol == 'uart' and provider == 'lg69t':            connection = config.get('connection')            options = UartOptions(connection.get(                'path'), int(connection.get('baudrate')))            transfer = LG69TUartTransfer(options)            transfer.on_initalize()            return transfer        if protocol == '100base-t1' and provider in ['ins401','ins402']:            transfer = INS401EthernetTransfer()            transfer.on_initalize()            return transfer        raise Exception('Unsupported can transfer configuration')class TransferCenterError:    _message: str    _with_error: bool    def __init__(self) -> None:        self._message = ''        self._with_error = False    @property    def message(self):        return self._message    @property    def with_error(self):        return self._with_error    def set_error(self, value):        self._message = value        self._with_error = True    def clear_error(self, value):        self._with_error = Falseclass TransferCenter:    _error: TransferCenterError    _transfers: List[TransferBase]    def __init__(self, config) -> None:        self._error = TransferCenterError()        self._transfers = []        # build transfer from config        if not isinstance(config, list):            self._error.set_error('The configuration should be an array.')            return        for item in config:            if 'disabled' in item and item['disabled']:                continue            try:                transfer = TransferFactory.create(item)            except Exception as ex:                self._error.set_error(ex)                break            self._transfers.append(transfer)    def get_error(self):        if self._error.with_error:            return self._error        return None    def dispatch_vehicle_speed(self, speed):        for transfer in self._transfers:            transfer.send_vehicle_speed(speed)
+import struct
+import time
+import threading
+from typing import Any, List
+
+from ..typings import EthOptions, UartOptions
+from ..transfer.uart_transfer import UartTransfer
+from ..transfer.eth_100base_t1_transfer import Eth100BaseT1Transfer
+from ..devices.eth_device import (select_ethernet_interface, collect_devices)
+from . import (create_transfer, create_eth_100base_t1_transfer)
+from ..common import (message_helper, utils, app_logger)
+
+def get_device_uart_transfer(provider:str, options: UartOptions):
+    if provider == 'lg69t':
+        return LG69TUartTransfer(options)
+    elif provider == 'rtk350la':
+        return RTK350LATransfer(options)
+    else:
+        return None
+
+class TransferBase:
+    def __init__(self) -> None:
+        super().__init__()
+
+    def on_initalize(self):
+        pass
+
+    def send(self, speed: int):
+        pass
+
+class RTK350LATransfer(TransferBase):
+    _transfer: UartTransfer
+
+    def __init__(self, options: UartOptions) -> None:
+        super().__init__()
+        self._transfer = create_transfer(options)
+
+        if not self._transfer:
+            raise Exception(
+                'Cannot connect the RTK350LA Uart transfer, please check the configuration')
+
+    def on_initalize(self):
+       pass
+
+    def send_vehicle_speed(self, speed: int):
+        # build uart command cA, speed
+        message = message_helper.build_uart_command(bytes([0x63, 0x41]),list(struct.pack("<f", speed)))
+        print(message)
+        #self._transfer.send(message)
+
+
+class LG69TUartTransfer(TransferBase):
+    _transfer: UartTransfer
+
+    def __init__(self, options: UartOptions) -> None:
+        super().__init__()
+        self._transfer = create_transfer(options)
+
+        if not self._transfer:
+            raise Exception(
+                'Cannot connect the LG69T Uart transfer, please check the configuration')
+
+    def on_initalize(self):
+        # set uart transfer mode
+        message = message_helper.build_nmea_message(
+            [
+                'PQTMCFGDRMODE',
+                '1',
+                '2'
+            ])
+        self._transfer.send(message)
+        time.sleep(0.1)
+        # save configuration
+        message = message_helper.build_nmea_message(
+            [
+                'PQTMSAVEPAR'
+            ])
+        self._transfer.send(message)
+
+        # start a thread to receive message from uart
+        threading.Thread(target=self.__listen_data).start()
+
+    def send_vehicle_speed(self, speed: int):
+        # write uart message
+        # build NMEA format message
+        actual = round(speed*1000/3600,4)
+        message = message_helper.build_nmea_message(
+            [
+                'PQTMVEHMSG',
+                '1',
+                '0',
+                str(actual)
+            ])
+        self._transfer.send(message)
+
+    def __listen_data(self):
+        log_file = app_logger.create_raw_file_logger('lg69t_raw')
+        try:
+            while True:
+                data = self._transfer.recv()
+                # write to log file
+                log_file.append(data)
+                time.sleep(0.1)
+        except KeyboardInterrupt as ex:
+            return
+
+
+class INS401EthernetTransfer(TransferBase):
+    _transfer: Eth100BaseT1Transfer
+    _src_mac: str
+    _dst_mac_addresses: List[str]
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        iface = select_ethernet_interface()
+        self._src_mac = iface.mac
+
+        eth_devices = collect_devices(iface, self._src_mac)
+        config = utils.get_config()
+
+        if config["devices_mac"]:
+            self._dst_mac_addresses = config["devices_mac"]
+        else:
+            self._dst_mac_addresses = [
+                device.mac_address for device in eth_devices]
+
+        self._transfer = create_eth_100base_t1_transfer(EthOptions(
+            iface,
+            self._src_mac,
+            self._dst_mac_addresses))
+
+        if not self._transfer:
+            raise Exception(
+                'Cannot connect the INS 100base-t1 transfer, please check the configuration')
+
+    def on_initalize(self):
+        print('[Info] Listen mac address list:')
+        if len(self._dst_mac_addresses) == 0:
+            print('[Info] No device detected')
+
+        for address in self._dst_mac_addresses:
+            print('MAC: {0}, SN: {1}'.format(
+                address, utils.convert_mac_to_sn(address)))
+
+    def send_vehicle_speed(self, speed: int):
+        def build_eth_commands(devices_mac, local_mac, packet_type_bytes, message_bytes):
+            commands = []
+            for dest_mac in devices_mac:
+                command = message_helper.build_eth_command(dest_mac, local_mac,
+                                                           packet_type_bytes,
+                                                           message_bytes)
+                commands.append(command)
+
+            return commands
+
+        commands = build_eth_commands(self._dst_mac_addresses,
+                                      self._src_mac,
+                                      bytes([0x01, 0x0b]),
+                                      list(struct.pack("<f", speed)))
+
+        if self._transfer:
+            self._transfer.send_batch(commands)
+
+
+class TransferFactory:
+    def create(config):
+        protocol = config.get('protocol')
+        provider = config.get('provider')
+
+        if protocol == 'uart':
+            connection = config.get('connection')
+            options = UartOptions(connection.get(
+                'path'), int(connection.get('baudrate')))
+            transfer = get_device_uart_transfer(provider, options)
+            if not transfer:
+                raise Exception(
+                    'Cannot connect the {0} Uart transfer, please check the configuration'.format(provider))
+
+            transfer.on_initalize()
+            return transfer
+
+        if protocol == '100base-t1' and provider in ['ins401','ins402']:
+            transfer = INS401EthernetTransfer()
+            transfer.on_initalize()
+            return transfer
+
+        raise Exception('Unsupported can transfer configuration')
+
+
+class TransferCenterError:
+    _message: str
+    _with_error: bool
+
+    def __init__(self) -> None:
+        self._message = ''
+        self._with_error = False
+
+    @property
+    def message(self):
+        return self._message
+
+    @property
+    def with_error(self):
+        return self._with_error
+
+    def set_error(self, value):
+        self._message = value
+        self._with_error = True
+
+    def clear_error(self, value):
+        self._with_error = False
+
+
+class TransferCenter:
+    _error: TransferCenterError
+    _transfers: List[TransferBase]
+
+    def __init__(self, config) -> None:
+        self._error = TransferCenterError()
+        self._transfers = []
+        # build transfer from config
+        if not isinstance(config, list):
+            self._error.set_error('The configuration should be an array.')
+            return
+
+        for item in config:
+            if 'disabled' in item and item['disabled']:
+                continue
+
+            try:
+                transfer = TransferFactory.create(item)
+            except Exception as ex:
+                self._error.set_error(ex)
+                break
+
+            self._transfers.append(transfer)
+
+    def get_error(self):
+        if self._error.with_error:
+            return self._error
+        return None
+
+    def dispatch_vehicle_speed(self, speed):
+        for transfer in self._transfers:
+            transfer.send_vehicle_speed(speed)
